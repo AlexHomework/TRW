c933e8316f9f9c65652ba46fb406b2351c81c640
Alex <theworldcreator@gmail.com>
2012-10-01 17:12:23 +0400

    A lot of typo fixes, it can be compiled now

--- trwGridPotts.m
+++ trwGridPotts.m
@@ -1,72 +1,47 @@
 function [labels, energy, lowerBound, time] = trwGridPotts(unary, vertC, horC, metric)
-
-% chain_length = N * M;
-% lambda_unary           = ones(chain_length, K);
-% lambda_binary          = ones(chain_length - 1, K, K);
-
-% chain_unary            = zeros(T, chain_length, K);
-% % Horizontal snake, unary potentials
-% for i = 1:N
-% 	if (rem(i,2) == 0)
-% 		% Even row
-% 		chain_unary(1, (i - 1)*M + 1:i*M, :) = fliplr(unary(i, :));
-% 	else
-% 		% Odd row
-% 		chain_unary(1, (i - 1)*M + 1:i*M, :) = unary(i, :);
-% 	end
-% end
-
-% % Vertical snake, unary potentials
-% for j = 1:M
-% 	if (rem(j,2) == 0)
-% 		% Even row
-% 		chain_unary(1, (j - 1)*N + 1:j*N, :) = fliplr(unary(:, j));
-% 	else
-% 		% Odd row
-% 		chain_unary(T, (j - 1)*N + 1:j*N, :) = unary(:, j);
-% 	end
-% end
-
-% chain_binary           = zeros(T, chain_length - 1, K, K);
-% % Horizontal snake, binary potentials
-% for j = 1:M
-% 	if (rem(j,2) == 0)
-% 		% Even row
-% 		chain_unary(1, (j - 1)*N + 1:j*N, :) = fliplr(unary(:, j));
-% 	else
-% 		% Odd row
-% 		chain_unary(T, (j - 1)*N + 1:j*N, :) = unary(:, j);
-% 	end
-% end
-% lambda_binary          = ones(chain_length - 1, K, K);
-% lambda_binary(2, :, :) = -1 * lambda_binary;
-% y = zeros(T, chain_length);
+[N, M, K] = size(unary);
 
 hor_y = zeros(N, M);
-
-for iteration = 1:3
+lambda_unary_hor = zeros(N, M, K);
+ver_y = zeros(N, M);
+lambda_unary_ver = zeros(N, M, K);
+lower_energy_arr = [];
+upper_energy_arr = [];
+for iteration = 1:10
 	alpha_n = 0.5;
 
 	% Y minimization
 	% The lower energy estimate
-	% lower_energy = 0;
+	lower_energy = 0;
 	% Horizontal chains
 	for chain_i = 1:N
-		[sub_en, hor_y(chain_i, :)] = minimize_chain(lambda_unary_hor(cain_i, :), horC(chain_i), metric);
-		% lower_energy += sub_en;
+		chain_unary = reshape(unary(chain_i, :, :), M, K) + reshape(lambda_unary_hor(chain_i, :, :), M, K);
+		[sub_en, hor_y(chain_i, :)] = minimize_chain(chain_unary, horC(chain_i, :), metric);
+		lower_energy = lower_energy + sub_en;
 	end
 
 	% Vertical chains
 	for chain_i = 1:M
-		[sub_en, ver_y(:, chain_i)] = minimize_chain(lambda_unary_ver(:, chain_i), verC(chain_i), metric);
-		% lower_energy += sub_en;
+		chain_unary = reshape(unary(:, chain_i, :), N, K) + reshape(lambda_unary_ver(:, chain_i, :), N, K);
+		[sub_en, ver_y(:, chain_i)] = minimize_chain(chain_unary, vertC(:, chain_i), metric);
+		lower_energy = lower_energy + sub_en;
 	end
 
-	upper_energy = min(gridEnergy(hor_y), gridEnergy(ver_y));
+	upper_energy = min(gridEnergy(unary, vertC, horC, metric, hor_y), gridEnergy(unary, vertC, horC, metric, ver_y));
 
+	lower_energy_arr = [lower_energy_arr, lower_energy];
+	upper_energy_arr = [upper_energy_arr, upper_energy];
 
-	% Lambda subgradient maximisation
-	lambda_unary_ver += alpha_n * (ver_y - hor_y);
-	lambda_unary_hor += alpha_n * (hor_y - ver_y);
+	% % Lambda subgradient maximisation
+	for p = 1:K
+		lambda_unary_hor(:, :, p) = lambda_unary_hor(:, :, p) + alpha_n * ((hor_y == p) - (ver_y == p));
+		lambda_unary_ver(:, :, p) = lambda_unary_ver(:, :, p) + alpha_n * ((ver_y == p) - (hor_y == p));
+	end
+	
 end
+
+plot(upper_energy_arr);
+hold on;
+plot(lower_energy_arr, 'r');
+
 end

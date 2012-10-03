84d3695f4416e5a2fd66fcf64f4dd78c4ef54344
Alex <theworldcreator@gmail.com>
2012-10-03 12:33:30 +0400

    Optimization: by not using reshape program became two times faster

--- minimize_chain.m
+++ minimize_chain.m
@@ -6,20 +6,24 @@ N = size(unary, 1);
 % Possible y values: from 1 to K
 K = size(metric, 1);
 
+idx_matrix = ones(1, K); % we will use this matrix as index inspite of using repmat
+
 % message_forward(i) it's message from i to i + 1
 message_forward = zeros(N - 1, K);
 
-message_forward(1, :) = min(repmat(unary(1, :), K, 1) + C(1) * metric, [], 2)';
+message_forward(1, :) = min(unary(idx_matrix, :) + C(1) * metric, [], 2)';
 for t = 2:N-1
-	message_forward(t, :) = min(repmat(unary(t, :) + message_forward(t - 1, :), K, 1) + C(t) * metric, [], 2)';
+	el = unary(t, :) + message_forward(t - 1, :);
+	message_forward(t, :) = min(el(idx_matrix, :) + C(t) * metric, [], 2)';
 end
 
 % message_backward(i) it's message from i to i - 1
 message_backward = zeros(N, K);
 
-message_backward(N, :) = min(repmat(unary(N, :), K, 1) + C(N - 1) * metric, [], 2)';
+message_backward(N, :) = min(unary(N * idx_matrix, :) + C(N - 1) * metric, [], 2)';
 for t = N-1:-1:2
-	message_backward(t, :) = min(repmat(unary(t, :) + message_backward(t + 1, :), K, 1) + C(t - 1) * metric, [], 2)';
+	el = unary(t, :) + message_backward(t + 1, :);
+	message_backward(t, :) = min(el(idx_matrix, :) + C(t - 1) * metric, [], 2)';
 end
 
 y = zeros(1, N);

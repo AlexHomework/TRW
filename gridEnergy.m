5d8115ad31a09b1531a0d7e7ca93447ead974bf1
Alex <theworldcreator@gmail.com>
2012-10-09 16:54:04 +0400

    Super fast vectorization

--- gridEnergy.m
+++ gridEnergy.m
@@ -1,23 +1,23 @@
 function [energy] = gridEnergy(unary, vertC, horC, metric, y)
-	energy = 0;
 	[K, N, M] = size(unary);
-	for i = 1:N
-		for j = 1:M
-			energy = energy + unary(y(i, j), i, j);
+	% It's tricky vectorised equivalent to the code below
+	energy = sum(unary(y(:) + K * (0 : N*M - 1)'));
+	energy = energy + horC(:)' * metric(y(1 : N*(M - 1)) + (y(N+1 : end) - 1) * K)';
+	y_w_f = y(2:end, :); % y without first row
+	y_w_l = y(1:N-1, :); % y without last row
+	energy = energy + vertC(:)' * metric(y_w_l(:) + (y_w_f(:) - 1) * K);
 
-			% Edges
-			if j > 1
-				energy = energy + horC(i, j - 1) * metric(y(i, j - 1), y(i, j));
-			end
-			if j < M
-				energy = energy + horC(i, j) * metric(y(i, j), y(i, j + 1));
-			end
-			if i > 1
-				energy = energy + vertC(i - 1, j) * metric(y(i - 1, j), y(i, j));
-			end
-			if i < N
-				energy = energy + vertC(i, j) * metric(y(i, j), y(i + 1, j));
-			end
-		end
-	end
+	% energy = 0;
+	% for i = 1:N
+	% 	for j = 1:M
+	% 		energy = energy + unary(y(i, j), i, j);
+	% 		% Edges
+	% 		if j > 1
+	% 			energy = energy + horC(i, j - 1) * metric(y(i, j - 1), y(i, j));
+	% 		end
+	% 		if i > 1
+	% 			energy = energy + vertC(i - 1, j) * metric(y(i - 1, j), y(i, j));
+	% 		end
+	% 	end
+	% end
 end

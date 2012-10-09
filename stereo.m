488143836226201839fb7f9c36d39796dacdd316
Alex <theworldcreator@gmail.com>
2012-10-09 16:57:52 +0400

    Useful trw launcher

---
+++ stereo.m
@@ -0,0 +1,7 @@
+function [disparity] = stereo(name)
+	filename = strcat('datasets/', name);
+	left = RGB2Luv(imread(strcat(filename, '1.png')));
+	right = RGB2Luv(imread(strcat(filename, '2.png')));
+	[unary, vertC, horC, metric] = potentials(left, right);
+	labels = trwGridPotts(unary, vertC, horC, metric);
+end

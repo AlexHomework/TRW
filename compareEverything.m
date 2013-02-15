72cf6728cca42e36006d93cf8829f89159a67728
Alex <theworldcreator@gmail.com>
2013-02-15 19:02:56 +0400

    Add ability to start compareEverything from existing experiment folder to recompute not everything, but only essential

--- compareEverything.m
+++ compareEverything.m
@@ -5,68 +5,89 @@ function compareEverything(experiment_name)
 	% 
 
 	experiment_folder = strcat('experiments/', experiment_name);
-	if exist(experiment_folder, 'dir') 
-		disp('Experiment with such name already exist!');
-		return;
-	end
-
-	mkdir(experiment_folder);
 
 	data_set = getDataSet();
+	baseline_algos = getBaselineAlgos();
+	step_algos = getStepComputingAlgos();
+	if exist(experiment_folder, 'dir')
+		% Find that we have already computed
+		processed_filename = strcat(experiment_folder, '/processed_list.mat');
+		load(processed_filename, 'data_set_names', 'baseline_algos_names', 'step_algos_names');
+	else
+		data_set_names = {};
+		baseline_algos_names = {};
+		step_algos_names = {};
+		mkdir(experiment_folder);
+	end
+
 	for data_piece_ind = 1:length(data_set)
 		data_piece = data_set{data_piece_ind};
 		unary = data_piece.unary;
 		vertC = data_piece.vertC;
 		horC = data_piece.horC;
 		data_name = data_piece.name;
+		% known_data_piece == true iff current data piece
+		% was already processed with some algorithms.
+		known_data_piece = sum(ismember(data_set_names, data_name)) == 1;
 
-		labels = cell(1);
-		energy = cell(1);
-		oracle_calls = cell(1);
-		time = cell(1);
-		step = cell(1);
-		lowerBound = cell(1);
-		legend_names = cell(1);
-		plot_num = 1;
+		if (known_data_piece)
+			results_filename = strcat(experiment_folder, '/data_', data_name, '.mat');
+			load(results_filename, 'labels', 'energy', 'oracle_calls', 'time', 'step', 'lowerBound', 'legend_names');
+			plot_num = length(labels) + 1;
+		else
+			% Start everything from scratch.
+			labels = cell(1);
+			energy = cell(1);
+			oracle_calls = cell(1);
+			time = cell(1);
+			step = cell(1);
+			lowerBound = cell(1);
+			legend_names = cell(1);
+			plot_num = 1;
+		end
 
-		baseline_algos = getBaselineAlgos();
 		for algo_i = 1:length(baseline_algos)
 			algo = baseline_algos{algo_i}{1};
-			[curr_labels, curr_energy, curr_lowerBound, curr_time] = algo(unary, vertC, horC);
-			labels{plot_num} = curr_labels;
-			energy{plot_num} = curr_energy;
-			lowerBound{plot_num} = curr_lowerBound;
-			time{plot_num} = curr_time;
-			step{plot_num} = [];
-			oracle_calls{plot_num} = [];
-			legend_names{plot_num} = baseline_algos{algo_i}{2};
-			plot_num = plot_num + 1;
+			algo_name = baseline_algos{algo_i}{2};
+			known_algo = sum(ismember(baseline_algos_names, algo_name)) == 1;
+			if (known_data_piece == false || known_algo == false)
+				[curr_labels, curr_energy, curr_lowerBound, curr_time] = algo(unary, vertC, horC);
+				labels{plot_num} = curr_labels;
+				energy{plot_num} = curr_energy;
+				lowerBound{plot_num} = curr_lowerBound;
+				time{plot_num} = curr_time;
+				step{plot_num} = [];
+				oracle_calls{plot_num} = [];
+				legend_names{plot_num} = algo_name;
+				plot_num = plot_num + 1;
+			end
 		end
 		% Set ylimits for making small size plots
-		small_upper = curr_energy(5);
-		small_lower = curr_lowerBound(5);
+		small_upper = energy{end}(5);
+		small_lower = lowerBound{end}(5);
 
 		% Now test all TRW variants
-		step_algos = getStepComputingAlgos();
 		for algo_i = 1:length(step_algos)
 			algo = step_algos{algo_i}{1};
 			init_params = step_algos{algo_i}{2};
-			[curr_labels, curr_energy, curr_lowerBound, ...
-						curr_time, curr_step, curr_oracle_calls] = ...
-						trwGridPotts(unary, vertC, horC, algo, init_params);
-			labels{plot_num} = curr_labels;
-			energy{plot_num} = curr_energy;
-			lowerBound{plot_num} = curr_lowerBound;
-			time{plot_num} = curr_time;
-			step{plot_num} = curr_step;
-			oracle_calls{plot_num} = curr_oracle_calls;
-			legend_names{plot_num} = step_algos{algo_i}{3};
-			plot_num = plot_num + 1;
+			algo_name = step_algos{algo_i}{3};
+			known_algo = sum(ismember(step_algos_names, algo_name)) == 1;
+			if (known_data_piece == false || known_algo == false)
+				[curr_labels, curr_energy, curr_lowerBound, ...
+							curr_time, curr_step, curr_oracle_calls] = ...
+							trwGridPotts(unary, vertC, horC, algo, init_params);
+				labels{plot_num} = curr_labels;
+				energy{plot_num} = curr_energy;
+				lowerBound{plot_num} = curr_lowerBound;
+				time{plot_num} = curr_time;
+				step{plot_num} = curr_step;
+				oracle_calls{plot_num} = curr_oracle_calls;
+				legend_names{plot_num} = step_algos{algo_i}{3};
+				plot_num = plot_num + 1;
+			end
 		end
 
 		plot_num = plot_num - 1;
-
-
 
 
 
@@ -150,7 +171,7 @@ function compareEverything(experiment_name)
 		plot_arr = [];
 		curr_legend_names = {};
 		for i = 1:plot_num
-			if (length(step{i}) > 0)
+			if (~isempty(step{i}))
 				curr_time = time{i};
 				add = (curr_time(end) + 1) : max_time;
 				add = reshape(add, length(add), 1);
@@ -162,7 +183,7 @@ function compareEverything(experiment_name)
 				plot_arr = [plot_arr, p];
 			end
 		end
-		figureProperties(plot_arr, curr_legend_names, 'Step plot', 'Step size', 'Energy');
+		figureProperties(plot_arr, curr_legend_names, 'Step plot', 'Iteration', 'Step size');
 		hold off;
 
 
@@ -180,7 +201,7 @@ function compareEverything(experiment_name)
 		plot_arr = [];
 		curr_legend_names = {};
 		for i = 1:plot_num
-			if (length(oracle_calls{i}) > 0)
+			if (~isempty(oracle_calls{i}))
 				curr_oracle_calls = oracle_calls{i};
 				curr_energy = energy{i};
 				p = plot(curr_oracle_calls, curr_energy);
@@ -207,9 +228,23 @@ function compareEverything(experiment_name)
 		close;
 
 
-		out_filename = strcat(experiment_folder, '/data.mat');
+		out_filename = strcat(experiment_folder, '/data_', data_name, '.mat');
 		save(out_filename, 'labels', 'energy', 'oracle_calls', 'time', 'step', 'lowerBound', 'legend_names');
 	end
+
+	% Store what methods & data we used here.
+	% This make possible to recognize already
+	% processed data and not recompute it in future.
+	data_set_mat = cell2mat(data_set);
+	data_set_names = {data_set_mat.name};
+	baseline_algos_mat = [baseline_algos{:}];
+	baseline_algos_names = {baseline_algos_mat(2:2:end)};
+	baseline_algos_names = baseline_algos_names{1};
+	step_algos_mat = [step_algos{:}];
+	step_algos_names = {step_algos_mat(3:3:end)};
+	step_algos_names = step_algos_names{1};
+	out_filename = strcat(experiment_folder, '/processed_list.mat');
+	save(out_filename, 'data_set_names', 'baseline_algos_names', 'step_algos_names');
 end
 
 function plotProperties(p)
